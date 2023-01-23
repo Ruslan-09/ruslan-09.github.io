68ac5183f86f1b84e2ba32fa960db2fb66341858
Ruslan <mrrus20@gmail.com>
2023-01-23 16:57:21 +0300

    add second table

--- 1.js
+++ 1.js
@@ -1,11 +1,16 @@
 let array = []
 
-function insert(contentId, buttonId) {
+function insertContent(contentId, buttonId) {
     for (item of ["about", "calc", "tableButton", "contact"]) {
         document.getElementById(item).classList.remove('selected');
     }
     document.getElementById(buttonId).classList.add('selected');
     document.getElementById("section1").innerHTML = document.getElementById(contentId).innerHTML
+}
+
+function insertTable (TableId) {
+    document.getElementById("section1").append(document.getElementById(TableId))
+    document.getElementById("tables1").innerHTML = document.getElementById(TableId).innerHTML
 }
 
 function addPet() {   
@@ -23,21 +28,44 @@ function addPet() {
         let td = document.createElement('td') 
         tr.append(td) 
         td.id = pet[it]
-        pet1[i] = document.getElementById(it).value
+        pet1.push(document.getElementById(it).value) 
         document.getElementById(it).value = ''
         td.innerHTML = pet1[i]
         i++
     }
 }
 
-function add(){
+function addPet2() {   
+    let pet = {
+                "kindInput2": "kindStr", 
+                "nameInput2": "nameStr", 
+                "colorInput2": "colorStr", 
+                "weightInput2": "weightStr"
+               }
+    let pet1 = []
+    let i = 0
+    let tr = document.createElement('tr') 
+    document.getElementById("tableBody2").append(tr) 
+    for (it of ["kindInput2", "nameInput2", "colorInput2", "weightInput2"]) {
+        let td = document.createElement('td') 
+        tr.append(td) 
+        td.id = pet[it]
+        td.innerHTML = document.getElementById(it)
+        pet1.push(document.getElementById(it).value) 
+        document.getElementById(it).value = ''
+        td.innerHTML = pet1[i]
+        i++
+    }
+}
+
+function addNumber() {
 	let number = document.getElementById("number").value
     array.push(Number(number))
     document.getElementById("array").innerHTML = array
     document.getElementById("number").value = ''
 }
 
-function calc(){
+function calc() {
     let sum = 0
     for (item of array) {
         sum += item
@@ -45,7 +73,7 @@ function calc(){
     document.getElementById("avarage").innerHTML = "Avarage = " + sum/array.length
 }
 
-function max(){
+function max() {
     let maxV = array[0]
     for (item of array){
         if (maxV < item){
@@ -55,7 +83,7 @@ function max(){
     document.getElementById("maxValue").innerHTML = "Max value = " + maxV
 }
 
-function min(){
+function min() {
     let minV = array[0]
     for (item of array){
         if (minV > item){
