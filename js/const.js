bf13fd45ba1dee6cfd3f6c766fb70471ad49c5e4
Ruslan <mrrus20@gmail.com>
2023-01-26 21:28:06 +0300

    translate, russian version

--- js/const.js
+++ js/const.js
@@ -1,25 +1,49 @@
-const EN_ABOUT_ME = `Equipping yourself with the right about us template can help you expertly lay out your vision faster. <br>
-That’s why our team gathered five templates to help you build the perfect about page.<br>
+
+let aboutEn = `Equipping yourself with the right "about us" template can help you expertly lay out your vision faster. <br>
+That's why our team gathered five templates to help you build the perfect about page.<br>
 Like other about pages, the about section of your website should include your mission, purpose, <br>
-and who’s on your team. But there are other best practices for crafting a stellar about page. <br><br>
-<strong>In this post, you’ll explore:</strong> <br>
+and who's on your team. But there are other best practices for crafting a stellar about page. <br><br>
+<strong>In this post, you'll explore:</strong> <br>
 <ul>
     <li>What makes a good about page?</li>
     <li>How to write an about page</li>
     <li>About us page design</li>
     <li>The Best About Us Page Templates and Examples</li>
     <li>The Best About Me Page Templates and Examples</li>
-</ul>`
-
-const RU_ABOUT_ME = `Снабдив себя правильным шаблоном о нас, вы сможете быстрее профессионально изложить свое видение. <br>
+</ul>
+`
+let aboutRu = `Снабдив себя правильным шаблоном "о нас", вы сможете быстрее профессионально изложить свое видение. <br>
 Вот почему наша команда собрала пять шаблонов, которые помогут вам создать идеальную страницу информации.<br>
 Как и на других страницах "О нас", раздел "О нас" на вашем веб-сайте должен содержать информацию о вашей миссии, целях, <br>
-и кто в твоей команде. Но есть и другие рекомендации по созданию звездной страницы «О себе». <br><br>
+и кто в вашей команде. Но есть и другие рекомендации по созданию звездной страницы «о себе». <br><br>
 <strong>В этом посте вы узнаете:</strong> <br>
 <ul>
-     <li>Чем хороша страница информации?</li>
-     <li>Как написать информацию о странице</li>
-     <li>Дизайн страницы о нас</li>
+     <li>Чем хороша страница информации о себе?</li>
+     <li>Как написать страницу информации о себе</li>
+     <li>Дизайн страницы "о нас"</li>
      <li>Лучшие шаблоны и примеры страниц о нас</li>
      <li>Лучшие шаблоны и примеры страницы «Обо мне»</li>
-</ul>`
+</ul>
+`
+let contactsEn = `
+    Adress: 3225. Sokak, Ciftlikkoy, Yenisehir, Mersin. <br>
+    Tel.: +90-536-017-33-98<br>
+    E-mail: Ruslan.Bairamuk.09@gmail.com <br>
+`
+let contactsRu = `
+    Адрес: ул. 3225, Чифлиткой, Енишехир, Мерсин. <br>
+    Tel.: +90-536-017-33-98<br>
+    E-mail: Ruslan.Bairamuk.09@gmail.com <br>
+`
+//choosenLanguege()
+
+function choosenLanguege() {
+    if (document.getElementById('lang').innerHTML === 'En') {
+        document.getElementById('paragraph1').innerHTML = aboutEn
+        document.getElementById('contacts').innerHTML = contactsEn
+    } else {
+        document.getElementById('paragraph1').innerHTML = aboutRu
+        document.getElementById('contacts').innerHTML = contactsRu
+    }
+    console.log(contactsEn)
+}
