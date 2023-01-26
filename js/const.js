b2482af79fd42b8fff369ada419f8b31e6e669e9
Ruslan <mrrus20@gmail.com>
2023-01-26 14:10:29 +0300

    new style, new table style, added lang switcher

---
+++ js/const.js
@@ -0,0 +1,25 @@
+const EN_ABOUT_ME = `Equipping yourself with the right about us template can help you expertly lay out your vision faster. <br>
+That’s why our team gathered five templates to help you build the perfect about page.<br>
+Like other about pages, the about section of your website should include your mission, purpose, <br>
+and who’s on your team. But there are other best practices for crafting a stellar about page. <br><br>
+<strong>In this post, you’ll explore:</strong> <br>
+<ul>
+    <li>What makes a good about page?</li>
+    <li>How to write an about page</li>
+    <li>About us page design</li>
+    <li>The Best About Us Page Templates and Examples</li>
+    <li>The Best About Me Page Templates and Examples</li>
+</ul>`
+
+const RU_ABOUT_ME = `Снабдив себя правильным шаблоном о нас, вы сможете быстрее профессионально изложить свое видение. <br>
+Вот почему наша команда собрала пять шаблонов, которые помогут вам создать идеальную страницу информации.<br>
+Как и на других страницах "О нас", раздел "О нас" на вашем веб-сайте должен содержать информацию о вашей миссии, целях, <br>
+и кто в твоей команде. Но есть и другие рекомендации по созданию звездной страницы «О себе». <br><br>
+<strong>В этом посте вы узнаете:</strong> <br>
+<ul>
+     <li>Чем хороша страница информации?</li>
+     <li>Как написать информацию о странице</li>
+     <li>Дизайн страницы о нас</li>
+     <li>Лучшие шаблоны и примеры страниц о нас</li>
+     <li>Лучшие шаблоны и примеры страницы «Обо мне»</li>
+</ul>`
--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+let array = []
+let x = 0
+
 function insertContent(contentId, buttonId) {
     for (item of ["about", "calc", "tableButton", "contact"]) {
         document.getElementById(item).classList.remove('selected')
@@ -6,8 +9,66 @@ function insertContent(contentId, buttonId) {
     document.getElementById("section1").innerHTML = document.getElementById(contentId).innerHTML
 }
 
-function insertTable (TableId) {
-    document.getElementById("section1").append(document.getElementById(TableId))
-    document.getElementById("tables1").innerHTML = document.getElementById(TableId).innerHTML
+function addPet() {   
+    let pet = {
+                "kindInput3": "kindStr", 
+                "nameInput3": "nameStr", 
+                "colorInput3": "colorStr", 
+                "weightInput3": "weightStr"
+               }
+    let tr = document.createElement('tr') 
+    document.getElementById("tableBody3").append(tr) 
+    for (it of ["kindInput3", "nameInput3", "colorInput3", "weightInput3"]) {
+        let td = document.createElement('td') 
+        tr.append(td) 
+        td.id = pet[it]
+        td.innerHTML = (document.getElementById(it).value.charAt(0).toUpperCase() + document.getElementById(it).value.slice(1))
+        console.log(document.getElementById(it).value)
+        document.getElementById(it).value = ''
+    }
+    x++
+    let td = document.createElement('td') 
+    tr.id = 'newLine'+x
+    td.id = 'newBox'+x
+    let but = document.createElement('button') 
+    but.innerHTML = 'Remove'
+    but.setAttribute("onclick", "this.parentElement.parentElement.remove()")
+    but.classList.add('table')
+    document.getElementById('newLine'+x).append(td) 
+    document.getElementById('newBox'+x).append(but) 
 }
 
+function addNumber() {
+	let number = document.getElementById("number").value
+    array.push(Number(number))
+    document.getElementById("array").innerHTML = array
+    document.getElementById("number").value = ''
+}
+
+function calc() {
+    let sum = 0
+    for (item of array) {
+        sum += item
+    }
+    document.getElementById("avarage").innerHTML = "Avarage = " + sum/array.length
+}
+
+function max() {
+    let maxV = array[0]
+    for (item of array){
+        if (maxV < item){
+            maxV = item
+        }
+    }
+    document.getElementById("maxValue").innerHTML = "Max value = " + maxV
+}
+
+function min() {
+    let minV = array[0]
+    for (item of array){
+        if (minV > item){
+            minV = item
+        }
+    }
+    document.getElementById("minValue").innerHTML = "Min value = " + minV
+}
